4bd7aabe861d5966567a2a1911f409882a463212
DudeGFA <favouridowuagida0@gmail.com>
2022-10-17 18:39:56 -0700

    fixed puppet-lint issues

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #!/usr/bin/pup
 #creates a file
 file { '/tmp/school':
-    content  => 'I love Puppet',
-    mode     => '0744',
-    owner    => 'www-data',
-    group    => 'www-data',
+    content => 'I love Puppet',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
 }
