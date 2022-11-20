73ca6746861a441132f5d933af4f4ff9c1f0ae3d
DudeGFA <favouridowuagida0@gmail.com>
2022-11-20 02:02:02 +0100

    created 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Returns information on todo list progress for an employee id"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    import csv
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos/',
+                         params={'userId': sys.argv[1]}).json()
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]).json()
+    filename = sys.argv[1] + ".csv"
+    with open(filename, 'w', newline="") as csvfile:
+        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            csvwriter.writerow([sys.argv[1], user['name'],
+                                todo['completed'], todo['title']])
+    completed_tasks = [task for task in todos if task["completed"] is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user['name'], len(completed_tasks), len(todos)))
+    [print("\t " + task["title"]) for task in completed_tasks]
